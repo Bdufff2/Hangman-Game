5b655c9473a36ea6b826cad9d82faeb795dfadf2
duff.brian21@gmail.com <duff.brian21@gmail.com>
2018-02-18 20:01:14 -0800

    Refactoring game

---
+++ assets/javascript/states.js
@@ -0,0 +1,155 @@
+const statesArray = {
+    Alabama: {
+      capital: "Montgomery"
+    },
+    Alaska: {
+      capital: "Juneau"
+    },
+    Arizona: {
+      capital: "Phoenix"
+    },
+    Arkansas: {
+      capital: "Little Rock"
+    },
+    California: {
+      capital: "Sacramento"
+    },
+    Colorado: {
+      capital: "Denver"
+    },
+    Connecticut: {
+      capital: "Hartford"
+    },
+    Delaware: {
+      capital: "Dover"
+    },
+    Florida: {
+      capital: "Tallahassee"
+    },
+    Georgia: {
+      capital: "Atlanta"
+    },
+    Hawaii: {
+      capital: "Honolulu"
+    },
+    Idaho: {
+      capital: "Boise"
+    },
+    Illinois: {
+      capital: "Springfield"
+    },
+    Indiana: {
+      capital: "Indianapolis"
+    },
+    Iowa: {
+      capital: "Des Moines"
+    },
+    Kansas: {
+      capital: "Topeka"
+    },
+    Kentucky: {
+      capital: "Frankfort"
+    },
+    Louisiana: {
+      capital: "Baton Rouge"
+    },
+    Maine: {
+      capital: "Augusta"
+    },
+    Maryland: {
+      capital: "Annapolis"
+    },
+    Massachusetts: {
+      capital: "Boston"
+    },
+    Michigan: {
+      capital: "Lansing"
+    },
+    Minnesota: {
+      capital: "St. Paul"
+    },
+    Mississippi: {
+      capital: "Jackson"
+    },
+    Missouri: {
+      capital: "Jefferson City"
+    },
+    Montana: {
+      capital: "Helena"
+    },
+    Nebraska: {
+      capital: "Lincoln"
+    },
+    Nevada: {
+      capital: "Carson City"
+    },
+    New Hampshire: {
+      capital: "Concord"
+    },
+    New Jersey: {
+      capital: "Trenton"
+    },
+    New Mexico: {
+      capital: "Santa Fe"
+    },
+    New York: {
+      capital: "Albany"
+    },
+    North Carolina: {
+      capital: "Raleigh"
+    },
+    North Dakota: {
+      capital: "Bismarck"
+    },
+    Ohio: {
+      capital: "Columbus"
+    },
+    Oklahoma: {
+      capital: "Oklahoma City"
+    },
+    Oregon: {
+      capital: "Salem"
+    },
+    Pennsylvania: {
+      capital: "Harrisburg"
+    },
+    Rhode Island: {
+      capital: "Providence"
+    },
+    South Carolina: {
+      capital: "Columbia"
+    },
+    South Dakota: {
+      capital: "Pierre"
+    },
+    Tennessee: {
+      capital: "Nashville"
+    },
+    Texas: {
+      capital: "Austin"
+    },
+    Utah: {
+      capital: "Salt Lake City"
+    },
+    Vermont: {
+      capital: "Montpelier"
+    },
+    Virginia: {
+      capital: "Richmond"
+    },
+    Washington: {
+      capital: "Olympia"
+    },
+    West Virginia: {
+      capital: "Charleston"
+    },
+    Wisconsin: {
+      capital: "Madison"
+    },
+    Wyoming: {
+      capital: "Cheyenne"
+    },
+  }
+  
+  
+  module.exports = statesArray;
